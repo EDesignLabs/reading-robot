c01e308f7d15048bb1ed0dcd96b490a317ba384e
luke <luke@2730bdff-1d18-4183-a0e6-d7181ba5eda7>
2012-03-16 02:18:53 +0000

    Structure setup and login screen
    git-svn-id: https://readbuddy.unfuddle.com/svn/readbuddy_readbud/trunk@10 2730bdff-1d18-4183-a0e6-d7181ba5eda7

---
+++ web/front-end/js/login.js
@@ -0,0 +1,28 @@
+﻿$(function(){
+
+	$("#login input").keyup(function(event){
+		if(event.keyCode == 13){
+			$("#login .button").click();
+		}
+	});
+
+	$("#login .button").click(function(){
+	
+		var screen = $("#login");
+	
+		screen.css("position","relative");
+		screen.animate({left: 0-screen.outerWidth() - 100});
+		console.log("test");
+	});
+	
+	
+	$("#login input").focus(function() {
+
+		if( $(this).val() == "name?" ) {
+			$(this).val("");
+		}
+
+	});
+
+
+});
