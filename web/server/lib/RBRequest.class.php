e1f38ed29104a9ebd68f8481355ffe13def746b7
justin <justin@2730bdff-1d18-4183-a0e6-d7181ba5eda7>
2012-03-22 18:59:21 +0000

    Updated api calls. Added upload data endpoint.
    git-svn-id: https://readbuddy.unfuddle.com/svn/readbuddy_readbud/trunk@67 2730bdff-1d18-4183-a0e6-d7181ba5eda7

--- web/server/lib/RBRequest.class.php
+++ web/server/lib/RBRequest.class.php
@@ -1,33 +1,56 @@
 <?
 //Request class handling api requests
 require_once('includes/mysql_connect.php');
+
 class RBRequest
 {
 
-    public function __construct()
-    {
-        
-    }
-    
-    public function getEntriesForUser( $user )
-    {
-           $sql      = 'SELECT entries FROM users WHERE user="'.$user.'"';
-           $response = mysql_query($sql);
-    }
-    
-    public function createUser( $user , $password )
-    {
-		//TODO: only allow for uniuq "user" entries
-        $sql = "INSERT INTO rb_users (username, password) VALUES ('$user','$password')";
-        $response = mysql_query($sql);
-		echo $sql;
-        if ( $response )  
-			return "success";
+	public function __construct()
+	{
+
+	}
+
+	public function getEntriesForUser( $user )
+	{
+		$sql      = 'SELECT entries FROM users WHERE user="'.$user.'"';
+		$response = mysql_query($sql);
+
+	}
+
+	public function getUser ( $user ) 
+	{
+		$sql      = 'SELECT * FROM rb_users WHERE username="'.$user.'"';
+		$response = mysql_query($sql);		
+		if ( mysql_num_rows($response) > 0 ) 
+			return 1; 
 		else 
-			return "fail";
-           
-    }
-    
+			return 0;
+	}
+	
+	public function checkPassword( $user, $password)
+	{
+		$sql      = 'SELECT password FROM rb_users WHERE username="'.$user.'"';
+		$response = mysql_query($sql);		
+		$pass = mysql_fetch_assoc($response);
+		if ( $pass['password'] == $password)
+			return 1;
+		else 
+			return 0;
+	}
+
+	public function createUser( $user , $password )
+	{
+		//TODO: only allow for uniuq "user" entries
+		$hash = md5($user);
+		$sql = "INSERT INTO rb_users (username, password, uid) VALUES ('$user','$password','$hash')";
+		$response = mysql_query($sql);
+		if ( $response )  
+			return 1;
+		else 
+			return 0;
+
+	}
+
 }
 
 ?>
--- web/server/request.php
+++ web/server/request.php
@@ -1,6 +1,56 @@
 <?php
+ini_set('display_errors',1);
+error_reporting(E_ALL|E_STRICT);
 //api root touchpoint
 require_once("lib/RBRequest.class.php");
+$query = $_GET['query'];
 $request = new RBRequest();
-$request->createUser("user", "password");
+
+
+switch ( $query ) 
+{
+	case "getUser":
+		$username = $_GET["user"];
+		$query    = $request->getUser($username);	
+		echo $query;
+		break;
+	case "createUser":
+		$username = $_GET["user"];
+		$password = $_GET["password"];
+		$query    = $request->createUser($username, $password);	
+		echo $query;
+		break;
+	case "checkPassword":
+		$username = $_GET["user"];
+		$password = $_GET["password"];
+		$query = $request->checkPassword( $username, $password);	
+		echo $query;
+		break;	
+	case "uploadData":
+		
+		break;
+	default:
+		echo "give me something to do";
+		break;
+}
+
+function dataUpload()
+{
+	$name = $_POST['name'];
+	$uid = "null";//$_POST['uid']";
+	$pid = $_POST['pid'];	
+	$type = $_POST['type'];		
+	$fileurl 	 = rawurlencode(basename( $_FILES['uploadedfile']['name']));
+	$extension   = substr(strrchr(basename( $_FILES['uploadedfile']['name']), '.'), 1);
+	$target_path = "files/";
+	$target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
+	if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) 
+	{
+	  	echo "file: " . basename( $_FILES['uploadedfile']['name']);
+		$sql = 'INSERT INTO rb_content (name, pid, uid) VALUES ("'.$_FILES['uploadedfile']['name'].'","'.$filesize.'","'.$extension.'","genre","'.$fileurl.'", "1")';
+		$query = mysql_query($sql);
+	 } else{
+	  echo "null";
+	 }
+}
 ?>
