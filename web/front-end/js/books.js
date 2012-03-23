bacb10a54a0fa472f90b1ab84f52772f694f98db
luke <luke@2730bdff-1d18-4183-a0e6-d7181ba5eda7>
2012-03-23 01:47:25 +0000

    Monster popup
    git-svn-id: https://readbuddy.unfuddle.com/svn/readbuddy_readbud/trunk@68 2730bdff-1d18-4183-a0e6-d7181ba5eda7

---
+++ web/front-end/js/books.js
@@ -0,0 +1,31 @@
+﻿$(function(){
+
+	$('#books a').click(function(){
+		var link = $(this).attr('href');
+		swipeTo($('#books'),$(link));
+		
+		$(link).flexslider({
+			animation: "slide",              //String: Select your animation type, "fade" or "slide"
+			slideDirection: "horizontal",   //String: Select the sliding direction, "horizontal" or "vertical"
+			slideshow: false, 
+			controlsContainer: link + " .controls",
+			animationLoop: false
+		});
+		
+		$(".callout").click(function () {
+			$(this).fadeTo('slow', 0);
+			$(".monster-callout").fadeTo('fast', 1);
+			$(".monster-callout .text").effect( "bounce", {"times": 2}, 150);
+			
+			
+			$(".monster-callout .text").html($($(this).attr('href')).first().html());
+		});
+
+		
+		
+		
+		return false;
+	});
+
+
+});
---
+++ web/front-end/js/global.js
@@ -0,0 +1,13 @@
+﻿
+function swipeTo(startScreen, endScreen){
+	
+	startScreen.css("position","relative");
+	startScreen.animate({left: 0-$('body').outerWidth() - 100}, 500, function() {
+		startScreen.css("display","none");
+		endScreen.css("position","relative");
+		endScreen.css("left",$('body').outerWidth());
+		endScreen.css("display","block");
+		endScreen.animate({left: 0});
+	});
+	
+}
