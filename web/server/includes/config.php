608a6012c0aa3a4824f6dccdddc78136deb61068
justin <justin@2730bdff-1d18-4183-a0e6-d7181ba5eda7>
2012-03-24 18:56:58 +0000

    Changed server settings.
    git-svn-id: https://readbuddy.unfuddle.com/svn/readbuddy_readbud/trunk@76 2730bdff-1d18-4183-a0e6-d7181ba5eda7

--- web/server/includes/config.php
+++ web/server/includes/config.php
@@ -1,13 +1,14 @@
 <?php
+/*
 define("DBSERVER", ":/Applications/MAMP/tmp/mysql/mysql.sock");
 define("DBNAME", "ReadingBuddy");
 define("DBUSER", "root");
 define("DBPASS", "root");
+*/
 
-/*
 define("DBSERVER", "localhost");
 define("DBNAME", "aphescom_dtc");
 define("DBUSER", "aphescom_justin");
 define("DBPASS", "aphescom_justin");
-*/
+
 ?>
